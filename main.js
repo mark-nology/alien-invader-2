c41b379191fb08b69efed71a0c4187bb06db520f
mark <mark@nology.io>
2019-07-01 12:54:37 +0100

    two aliens move around repeatedly, but so far not I'm not able to crete more

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ class Alien {
     get x() { return this.pos.x; }
     get y() { return this.pos.y; }
     // I'm having trouble with eg 'set x' ended up in an infinite loop, used in AlienMovement.moveAlien()
-    setx(xx) { this.pos.x = xx; }
-    sety(yy) { this.pos.y = yy; }
+    set x(x) { this.pos.x = x; }
+    set y(y) { this.pos.y = y; }
     move() { AlienMovement.moveAlien(this); }
 }
 
@@ -16,16 +16,18 @@ class Game {
     constructor(alienNames ) {
         this.uiLayer = new DivImpl();
         this.aliens = alienNames.map(name => new Alien(name, this.uiLayer.getRandomPosition() )); // use map to transform invaderNames to array of Alien classes
-        this.alienCount = alienNames.count;
+        this.alienCount = alienNames.length;
     }
     initialRender() {
         this.aliens.forEach(alien => DivImpl.createAndRenderAlien(alien)); // arrow functions tidy this up a lot
     }
     createAlien() {
         name = this.alienCount.toString();
-        this.aliens += new Alien(name, this.uiLayer.getRandomPosition() )
-        this.aliens.last();
+        this.aliens.push(new Alien(name, this.uiLayer.getRandomPosition() ) );
         this.alienCount++;
+        console.log(this.alienCount);
+        console.log(this.aliens);
+
     }
     moveAliens() {
         this.aliens.map(alien => {
@@ -39,13 +41,13 @@ class Game {
 class AlienMovement {
     static moveAlien(alien) {
         const direction = Math.floor(Math.random()*4);
-        console.log("direction ", direction);
+        // console.log("direction ", direction);
         // yeah the arithmetic could be better but I'm not interested in that atm
         switch (direction) {
-            case 0: alien.setx ( (alien.x+10) % 300); break;
-            case 1: alien.setx ( (alien.x-10) % 300); break;
-            case 2: alien.sety ( (alien.y+10) % 300); break;
-            case 3: alien.sety ( (alien.y-10) % 300); break;
+            case 0: alien.x = ( (alien.x+10) % 300); break;
+            case 1: alien.x = ( (alien.x-10) % 300); break;
+            case 2: alien.y = ( (alien.y+10) % 300); break;
+            case 3: alien.y = ( (alien.y-10) % 300); break;
         }
     }
 
@@ -70,36 +72,45 @@ class DivImpl extends  UILayer {
         super();
     }
     static createAndRenderAlien(alien) {
-        console.log("New ", alien);
+        console.log('createAndRenderAlien');
+        // console.log("New ", alien);
         DivImpl.createScreenAlien(alien);
         DivImpl.renderAlien(alien);
     }
     static renderAlien(alien) {
-        console.log(alien);
+        // console.log(alien);
         const screenAlienDiv = document.getElementById(alien.name);
         screenAlienDiv.style.left = alien.x.toString() + 'px';
         screenAlienDiv.style.top = alien.y.toString() + 'px';
-        console.log(screenAlienDiv);
-        console.log('------------');
+        // console.log(screenAlienDiv);
+        // console.log('------------');
     }
     static createScreenAlien(alien) {
+        console.log('createScreenAlien');
         var div = document.createElement("div");
         div.className = "alien";
         div.id = alien.name;
+        console.log(div);
         document.getElementById("gameArea").appendChild(div);
     }
 }
 
-function moveAliens() {
-    console.log('moveAliens called');
+let counter = 0;
+function runGame() {
+    // console.log('moveAliens called');
     game.moveAliens();
+    counter ++;
+    if (counter > 7) {
+        counter = 0;
+        game.createAlien();
+    }
 }
+
 
 const invaderNames = ["Zyglot", "Xev"];
 const game = new Game(invaderNames);
 game.initialRender();
-// I expected this to run forever
-setInterval(moveAliens(), 1000);
+setInterval(runGame, 200);
 
 
 
@@ -113,16 +124,16 @@ setInterval(moveAliens(), 1000);
 
   initCanvas() {
     this.canvas = document.getElementById("gameArea");
-    console.log(this.canvas);
+    // console.log(this.canvas);
     this.ctx = this.canvas.getContext("2d");
     [this.canvas.width, this.canvas.height] = [this.size, this.size]; // extracted to variable to make randomPosition easier, used destructuring syntax to show off a bit
     this.ctx.fillStyle = "#FF0000";
   }
 
   createAndRenderAlien(alien) {
-    console.log(alien);
+    // console.log(alien);
     this.ctx.fillRect(alien.x - 2, alien.y - 2, 4, 4);
-    console.log("Alien at ", alien.x, alien.y);
+    // console.log("Alien at ", alien.x, alien.y);
   }
 }*/
 
@@ -134,7 +145,7 @@ setInterval(moveAliens(), 1000);
     const rect = canvas.getBoundingClientRect();
     const x = event.clientX - rect.left;
     const y = event.clientY - rect.top;
-    console.log("x: " + x + " y: " + y);
+    // console.log("x: " + x + " y: " + y);
 }
 
 canvas.addEventListener('mousedown', function(e) {
